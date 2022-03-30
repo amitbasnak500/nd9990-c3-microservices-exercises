83eccb0a547bc6e91a8d81d47c4502c1d43c4aa8
Amit Basnak <amit.basnak@hotmail.com>
2022-03-29 21:26:23 -0500

    changeset1

--- project/udagram-deployment/docker/set_env.sh
+++ project/udagram-deployment/docker/set_env.sh
@@ -9,13 +9,3 @@ export AWS_REGION=us-east-1b
 export AWS_PROFILE=amit
 export JWT_SECRET=udagram
 export URL=http://localhost:8100
-
-setx POSTGRES_USERNAME amitbas
-setx POSTGRES_PASSWORD password
-setx POSTGRES_HOST cdr1.cqoaorq6dvcg.us-east-1.rds.amazonaws.com
-setx POSTGRES_DB postgres
-setx AWS_BUCKET arn:aws:s3:::amitbasbucket
-setx AWS_REGION us-east-1b
-setx AWS_PROFILE amit
-setx JWT_SECRET udagram
-setx URL http://localhost:8100
